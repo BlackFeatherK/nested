841fa7843104b49435108ab04db26c55804acbf3
BlackFeather <she3354248@yahoo.com.tw>
2016-12-26 21:16:36 +0800

    finish

---
+++ app/models/room_category.rb
@@ -0,0 +1,8 @@
+class RoomCategory < ApplicationRecord
+  belongs_to :hotel
+  validates :name,
+            presence: true
+  def to_s
+    name
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+ritz_carlton = Hotel.create(name: 'The Ritz-Carlton New York, Central Park')
+mandarin_oriental = Hotel.create(name: 'Mandarin Oriental, New York')
+['City View Guestroom', 'Deluxe Parkview Room'].each do |rc|
+  ritz_carlton.room_categories.create(name: rc)
+end
+['City View Room', 'Skyline View Room', 'Hudson River View Room'].each do |rc|
+  mandarin_oriental.room_categories.create(name: rc)
+end
